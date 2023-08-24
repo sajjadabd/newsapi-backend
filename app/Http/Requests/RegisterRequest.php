1765f0963a1d3a59f030f07db621c83e4db1bc84
sajjadabd <mynameissajjad@gmail.com>
2023-08-24 20:59:23 +0330

    create register request

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => [
+                'required',
+                'string',
+                Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised(),
+                'confirmed',
+            ]
+        ];
+    }
+}
