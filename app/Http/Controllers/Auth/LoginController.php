546d40b3de013718c6da3e321dddea2a4e36dca8
sajjadabd <mynameissajjad@gmail.com>
2023-08-24 18:22:36 +0330

    install passport

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,24 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+
+    // Login
+    public function login(Request $request)
+    {
+        // Validation logic here
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->intended('/dashboard');
+        } else {
+            return redirect()->back()->withErrors(['email' => 'Invalid credentials']);
+        }
+    }
+
+
+    // Logout
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    
+    // Registration
+    public function register(Request $request)
+    {
+        // Validation logic here
+        // Create a new user and save to the database
+        // Log in the user
+        return redirect()->intended('/dashboard');
+    }
+
+
 }
