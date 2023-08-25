e7c60bfadd2876594b6a8c2c364c4d617e16010b
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 18:34:44 +0330

    add getArticles function in ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    // Get articles based on user's preferences
+    public function getArticles(Request $request)
+    {
+        // Fetch articles from the database based on user preferences
+        // Return articles in the response
+    }
 }
