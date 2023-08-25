f5cb690c5e89790bcd4af04bd437f85166e00059
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 20:32:01 +0330

    set fields for source and category table

--- database/migrations/2023_08_25_150050_create_sources_table.php
+++ database/migrations/2023_08_25_150050_create_sources_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sources', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('category');
             $table->timestamps();
         });
     }
