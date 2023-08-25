3a4e89aad5e5707a5b3fa06aac29dff7d5bfcf03
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 10:38:46 +0330

    add access_token to users table and disable csrf token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('access_token')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use \App\Http\Controllers\AuthController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
