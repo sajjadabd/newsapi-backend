fc0c8fc468f7f99e5e1c7811c2abe65a5ff749bb
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 20:34:59 +0330

    make command for scrape newsapi

---
+++ app/Console/Commands/ScrapeNews.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ScrapeNews extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:scrape-news';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
