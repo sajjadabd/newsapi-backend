91cbfaab89d7cf3b285b0ae3b1ec291a1ba1a0ae
sajjadabd <mynameissajjad@gmail.com>
2023-08-25 18:33:54 +0330

    add functions for handling user prefrences

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // Get user's preferences
+    public function getPreferences(Request $request)
+    {
+        // Fetch user preferences from the database
+        // Return preferences in the response
+    }
+
+    // Update user's preferences
+    public function updatePreferences(Request $request)
+    {
+        // Validate request data
+        // Update user preferences in the database
+        // Return a success response
+    }
 }
