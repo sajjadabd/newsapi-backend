74805f286f0b9aaf2a9ab182515ebf7dcc0caefc
sajjadabd <mynameissajjad@gmail.com>
2023-08-26 00:28:48 +0330

    implement run seeder for categories table

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use \App\Models\Category;
+
 class CategorySeeder extends Seeder
 {
     /**
@@ -12,6 +14,20 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            ['title' => 'business'],
+            ['title' => 'entertainment'],
+            ['title' => 'general'],
+            ['title' => 'health'],
+            ['title' => 'science'],
+            ['title' => 'sports'],
+            ['title' => 'technology'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
+
+    // business entertainment general health science sports technology
 }
