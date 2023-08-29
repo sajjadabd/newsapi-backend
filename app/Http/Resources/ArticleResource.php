5e179cf988486fb84fba3c90bedef4e500969f83
sajjadabd <mynameissajjad@gmail.com>
2023-08-29 14:04:24 +0330

    create ArticleResource for returning diffForHumans

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'source' => $this->source,
+            'source_id' => $this->source_id,
+            'author' => $this->author,
+            'url' => $this->url,
+            'urlToImage' => $this->urlToImage,
+            'publishedAt' => $this->publishedAt,
+            'content' => $this->content,
+            'diffForHumans' => $this->created_at->diffForHumans();,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
